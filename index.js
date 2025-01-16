83091bdc90997284f188cebcc4b8fecb939d9de8
Amanda Pollard <Amandappollard@gmail.com>
2025-01-16 21:40:03 +0000

    Initial commit

--- index.js
+++ index.js
@@ -16,3 +16,32 @@ app.get('/', (req, res) => {
 const listener = app.listen(process.env.PORT || 3000, () => {
   console.log('Your app is listening on port ' + listener.address().port)
 })
+
+
+/*
+{
+  username: "fcc_test",
+  description: "test",
+  duration: 60,
+  date: "Mon Jan 01 1990",
+  _id: "5fb5853f734231456ccb3b05"
+}
+User:
+
+{
+  username: "fcc_test",
+  _id: "5fb5853f734231456ccb3b05"
+}
+Log:
+
+{
+  username: "fcc_test",
+  count: 1,
+  _id: "5fb5853f734231456ccb3b05",
+  log: [{
+    description: "test",
+    duration: 60,
+    date: "Mon Jan 01 1990",
+  }]
+}
+*/
